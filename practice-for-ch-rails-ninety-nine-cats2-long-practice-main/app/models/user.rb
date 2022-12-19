10262fd966514fcd549a827256b2d3d3bbd5a680
Zane <zeisen33@gmail.com>
2022-12-19 15:24:39 -0500

    token generators

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -3,11 +3,41 @@ class User < ApplicationRecord
     validates :password_digest, presence: true
     validates :session_token, presence: true
 
-    before_validation :set_token
+    before_validation :ensure_session_token
 
-    def set_token
-        self.session_token ||= SecureRandom::urlsafe_base64
+    def password= (new_pass)
+        self.password_digest = BCrypt::Password.create(new_pass) if is_password?(new_pass)
+        @password = new_pass
     end
 
-    
+    def is_password?(new_pass)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(new_pass)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else 
+            nil
+        end
+    end
+
+    def reset_session_token!
+        token = generate_unique_session_token
+        self.save!
+        return token
+    end 
+
+    private
+    def generate_unique_session_token
+        self.session_token = SecureRandom::urlsafe_base64
+    end
+
+    def ensure_session_token
+        if self.session_token = nil
+            generate_unique_session_token
+        end
+    end
 end
